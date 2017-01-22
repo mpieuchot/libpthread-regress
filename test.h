6471c0cc75d2780fdee0e5092c6bade7d1d0b027
Martin Pieuchot <mpi@openbsd.org>
2017-01-22 10:08:41 +1000

    Implement deprecated ftime(3) on top of gettimeofday(2).

--- test.h
+++ test.h
@@ -1,4 +1,6 @@
 
+#include <sys/time.h>
+
 #include <pthread.h>
 
 #include <assert.h>
@@ -8,3 +10,26 @@
 #include <unistd.h>
 
 #define Sleep(ms) usleep(ms * 1000)
+
+struct _timeb {
+	time_t		time;
+	uint16_t	millitm;
+	int16_t		timezone;
+	int16_t		dstflag;
+};
+
+static inline int
+_ftime(struct _timeb *tbp)
+{
+        struct timezone tz;
+        struct timeval t;
+
+        if (gettimeofday(&t, &tz) < 0)
+                return -1;
+        tbp->millitm = (uint16_t)(t.tv_usec / 1000);
+        tbp->time = t.tv_sec;
+        tbp->timezone = tz.tz_minuteswest;
+        tbp->dstflag = tz.tz_dsttime;
+
+	return 0;
+}
